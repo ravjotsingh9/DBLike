da2b5ae126ab9e440beb00abdd66b5f7f41381eb
elmali <leevalve@gmail.com>
2014-07-06 11:50:43 -0700

    db connection

---
+++ DBLike/Server/DatabaseAccess/DBConnection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.DatabaseAccess
+{
+    public class DBConnection
+    {
+        /// <summary>
+        /// Database Info
+        /// </summary>
+        //private string userName;
+        //private string password ;
+        //private string dataSource ;
+        public SqlConnection conn;
+        public SqlConnectionStringBuilder connString1Builder;
+
+        /// <summary>
+        ///  Constructor --- setup information for DB
+        /// </summary>
+        /// <param name="dbNumber">specift which DB you want to connect
+        ///                        e.g. 1 for database --- cics525Group3@n67s8dtv4s.database.windows.net
+        /// </param>
+        public DBConnection(int dbNumber)
+        {
+            DBNo(dbNumber);
+        }
+
+        /// <summary>
+        /// initialize database info according to the dbNumber
+        /// </summary>
+        /// <param name="number"></param>
+        public void DBNo(int number)
+        {
+            if (number == 1)
+            {
+                connString1Builder = new SqlConnectionStringBuilder();
+                connString1Builder.DataSource = "n67s8dtv4s.database.windows.net";
+                connString1Builder.InitialCatalog = "cics525Group3";
+                connString1Builder.Encrypt = true;
+                connString1Builder.TrustServerCertificate = false;
+                connString1Builder.UserID = "cics525@n67s8dtv4s";
+                connString1Builder.Password = "Account1*";
+                connString1Builder.ConnectTimeout = 60;
+            }
+
+        }
+
+        /// <summary>
+        /// Start Database connection
+        /// </summary>
+        /// <returns>
+        /// SqlConnection
+        /// </returns>
+        public SqlConnection DBConnect()
+        {
+            conn = new SqlConnection(connString1Builder.ToString());
+            conn.Open();
+            return conn;
+        }
+
+        /// <summary>
+        /// Close database connection 
+        /// </summary>
+        /// <param name="conn"> connection for database</param>
+        public void DBClose()
+        {
+            conn.Close();
+        }
+
+
+    }
+}
--- DBLike/Server/Program.cs
+++ DBLike/Server/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Server.DatabaseAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,8 +13,30 @@ namespace Server
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+        DBConnection test;
         static void Main()
         {
+            
+       
+            
+            try
+            {
+               test = new DBConnection(1);
+               test.DBConnect();
+               Console.WriteLine("yy");
+               Console.ReadLine();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                test.DBClose();
+            }
+            
+        
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
