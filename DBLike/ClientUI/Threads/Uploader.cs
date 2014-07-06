559c5adf572ffc21899ef5f2c451525c5c1694fc
ravjotsingh9 <ravjotsingh9@yahoo.com>
2014-07-06 14:46:24 -0700

    Added Design-Structure folders in Client UI

---
+++ DBLike/ClientUI/Threads/Uploader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientUI.Threads
+{
+    class Uploader
+    {
+        public void start()
+        {
+
+        }
+        public void stop()
+        {
+
+        }
+        private void threadStartFun()
+        {
+
+        }
+    }
+}
