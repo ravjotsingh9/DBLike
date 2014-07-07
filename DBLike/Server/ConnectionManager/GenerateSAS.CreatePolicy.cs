76b85a71c9cd2c0097e0900019fa354f8dc0dfda
elmali <leevalve@gmail.com>
2014-07-06 17:19:11 -0700

    blob sas uri

---
+++ DBLike/Server/ConnectionManager/GenerateSAS.CreatePolicy.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.ConnectionManager
+{
+    public partial class GenerateSAS
+    {
+        
+        /// <summary>
+        /// Create share accesss policy for READ, WRITE AND LIST
+        /// </summary>
+        /// <param name="blobClient"></param>
+        /// <param name="container"></param>
+        /// <param name="policyName"></param>
+        public void CreateSASRWL(CloudBlobClient blobClient, CloudBlobContainer container, string policyName)
+        {
+            policyName = "RWL";
+            
+            //Create a new stored access policy and define its constraints.
+            SharedAccessBlobPolicy sharedPolicy = new SharedAccessBlobPolicy()
+            {
+                SharedAccessExpiryTime = DateTime.UtcNow.AddHours(1),
+                Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write | SharedAccessBlobPermissions.List
+            };
+
+            //Get the container's existing permissions.
+            BlobContainerPermissions permissions = new BlobContainerPermissions();
+
+            //Add the new policy to the container's permissions.
+            permissions.SharedAccessPolicies.Clear();
+            permissions.SharedAccessPolicies.Add(policyName, sharedPolicy);
+            container.SetPermissions(permissions);
+        }
+    }
+}
---
+++ DBLike/Server/ConnectionManager/GenerateSAS.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server;
+
+namespace Server.ConnectionManager
+{
+    public partial class GenerateSAS
+    {
+        //private CloudBlobClient blobclient;
+
+        /// <summary>
+        /// Get Container's Sas Uri
+        /// </summary>
+        /// <param name="blobclient"></param>
+        /// <param name="container"></param>
+        /// <param name="policyName"></param>
+        /// <returns>Uri for container</returns>
+        public string GetContainerSasUri(CloudBlobClient blobclient, CloudBlobContainer container, string policyName)
+        {
+            
+            if (policyName == "RWL")
+            {
+                CreateSASRWL(blobclient, container, policyName);
+
+            }
+            
+            //Generate the shared access signature on the container. In this case, all of the constraints for the 
+            //shared access signature are specified on the stored access policy.
+            string sasContainerToken = container.GetSharedAccessSignature(null, policyName);
+
+            //Return the URI string for the container, including the SAS token.
+            return container.Uri + sasContainerToken;
+        }
+
+        
+
+    }
+}
