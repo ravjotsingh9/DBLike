50269764c2212dc3d6fa7a9e640a3183529e851c
ravjotsingh9 <ravjotsingh9@yahoo.com>
2014-07-08 07:48:07 -0700

    Made changes in Server Sign Up Msg Class

---
+++ DBLike/Server/MessageClasses/MsgSignUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.MessageClasses
+{
+    public class MsgSignUp
+    {
+        public class req
+        {
+            public string userName { get; set; }
+            public string psw { get; set; }
+        }
+
+        public class resp
+        {
+            // ack from server
+            public string ack { get; set; }
+        }
+    }
+}
