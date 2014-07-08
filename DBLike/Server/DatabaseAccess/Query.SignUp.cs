ca8d33e11ad6001644b285fb25ded60d0a456a21
hahnmiao <hahnmiao@gmail.com>
2014-07-08 16:48:37 -0700

    add signup query check user

---
+++ DBLike/Server/DatabaseAccess/Query.SignUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server.DatabaseAccess
+{
+    public partial class Query
+    {
+
+        public bool checkIfUserExists(string userName, SqlConnection sqlConnection)
+        {
+
+            try
+            {
+                SqlDataReader myReader = null;
+
+
+                SqlCommand myCommand = new SqlCommand("select * from dbo.Users where UserName = @UserName", sqlConnection);
+
+                myCommand.Parameters.AddWithValue("@UserName", userName);
+
+                myReader = myCommand.ExecuteReader();
+
+                // if user exists
+                if (myReader.Read())
+                {
+                    return true;
+                }
+
+                return false;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return true;
+            }
+            finally
+            {
+                sqlConnection.Close();
+
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
