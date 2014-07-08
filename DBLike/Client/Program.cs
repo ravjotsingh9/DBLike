2889b4fd7ba30c03313a683890f5b8fc18030da5
ravjotsingh9 <ravjotsingh9@yahoo.com>
2014-07-08 01:46:00 -0700

    Added icon as well as check if application is already running

--- DBLike/Client/Program.cs
+++ DBLike/Client/Program.cs
@@ -14,6 +14,11 @@ namespace Client
         [STAThread]
         static void Main()
         {
+            if(System.Diagnostics.Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Count() > 1)
+            {
+                MessageBox.Show("Sorry for Interrupt, but I guess the application is already running! :-)","Application Already Running");
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
