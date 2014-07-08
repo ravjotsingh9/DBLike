58a9ef52904ff77f964e761bce42f14a7b109cc7
ravjotsingh9 <ravjotsingh9@yahoo.com>
2014-07-08 08:16:51 -0700

    Structuring server

---
+++ DBLike/Server/Threads/Service-PollReq.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Threads
+{
+    class ServicePollReq
+    {
+        public void start()
+        {
+
+        }
+        public void stop()
+        {
+
+        }
+        private void threadStartFun()
+        {
+
+        }
+    }
+}
