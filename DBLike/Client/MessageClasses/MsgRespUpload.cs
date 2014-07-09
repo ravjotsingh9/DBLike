e78ca8cd07c483bc88518b7e8a124276ff76a48c
elmali <leevalve@gmail.com>
2014-07-08 21:28:38 -0700

    respupload

---
+++ DBLike/Client/MessageClasses/MsgRespUpload.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.MessageClasses
+{
+    public class MsgRespUpload
+    {
+        /**
+        public string type { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string filePathInSynFolder { get; set; }
+        public DateTime fileTimeStamps { get; set; }
+        public string fileHashValue { get; set; }
+        public string fileName { get; set; }
+         **/
+
+        public string indicator { get; set; }
+        public string filePathInSynFolder { get; set; }
+        public string fileContainerUri { get; set; }
+        public string fileBlobUri { get; set; }
+        public string type { get; set; }
+    }
+}
