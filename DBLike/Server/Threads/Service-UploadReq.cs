045dd60fc36c38ec04f45cf4620dfb3df30b8bce
elmali <leevalve@gmail.com>
2014-07-25 22:52:16 -0700

    delete comments

--- DBLike/Server/Threads/Service-UploadReq.cs
+++ DBLike/Server/Threads/Service-UploadReq.cs
@@ -32,13 +32,8 @@ namespace Server.Threads
                 //System.Windows.Forms.MessageBox.Show("uploader started:" + req, "Server");
                 //get the msg parse it
                 Server.Message.MessageParser parse = new Server.Message.MessageParser();
-                //MessageClasses.MsgUpload msgobj = new MessageClasses.MsgUpload();
-                //msgobj = parse.uploadParseMsg(req);
                 MessageClasses.MsgUpload upload = parse.uploadParseMsg(req);
-                /*
-                Console.WriteLine("userName: {0} password: {1}, sync folder patt: {2}, md5: {3}, timestamp: {4} fileName {5}",
-                                   upload.userName, upload.password, upload.filePathInSynFolder, upload.fileHashValue, upload.fileTimeStamps, upload.fileName);
-                **/
+                
                 //5 Server check the file info in the blob storage, if file has not existed, let the client to create the file
                 //  If file existed, check timestamp, hashvalue to see if client can upload
                 //  If allow upload, change hashvalue and timestamp in the blob storage
@@ -50,18 +45,6 @@ namespace Server.Threads
 
                     Blob blob = new Blob(blobClient, upload.userName, upload.filePathInSynFolder, upload.fileHashValue, upload.fileTimeStamps);
 
-
-                    /**
-                     Server.DatabaseAccess.Query query = new Server.DatabaseAccess.Query();
-                    if (!query.fileAlreadyExist(parse.userName, parse.filePathInSynFolder))
-                    {
-                        query.insertNewFileData(parse.userName, parse.fileName, parse.filePathInSynFolder, parse.fileHashValue, parse.fileTimeStamps);
-                    }
-                    else
-                    {
-                        query.updateFilesData(parse.userName, parse.filePathInSynFolder, parse.fileHashValue, parse.fileTimeStamps);
-                    }
-        **/
                     //6 Server Generate sas container/blob for the client. For basic upload, this case just generate container sas
                     //Server.ConnectionManager.BlobConn conn = new Server.ConnectionManager.BlobConn(1);
 
