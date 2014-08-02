e0f6a3177943a931abd8f793fe7f0022c33e2543
amulyave <amulyavelpula18@gmail.com>
2014-08-01 23:04:20 -0700

    Username validation updated

--- DBLike/Client/Form1.Designer.cs
+++ DBLike/Client/Form1.Designer.cs
@@ -30,16 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tbsignup = new System.Windows.Forms.TabPage();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.btnCreateAcctb2 = new System.Windows.Forms.Button();
-            this.btnBrowsetb2 = new System.Windows.Forms.Button();
-            this.txtfoldertb2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtpasstb2 = new System.Windows.Forms.TextBox();
-            this.txtusernametb2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.tbsignin = new System.Windows.Forms.TabPage();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.btnSignintb1 = new System.Windows.Forms.Button();
@@ -50,14 +40,25 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.tbsignup = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnCreateAcctb2 = new System.Windows.Forms.Button();
+            this.btnBrowsetb2 = new System.Windows.Forms.Button();
+            this.txtfoldertb2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtpasstb2 = new System.Windows.Forms.TextBox();
+            this.txtusernametb2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
-            this.tbsignup.SuspendLayout();
-            this.groupBox3.SuspendLayout();
             this.tbsignin.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.tbsignup.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -70,106 +71,8 @@
             this.tabControl1.Multiline = true;
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(349, 250);
+            this.tabControl1.Size = new System.Drawing.Size(360, 283);
             this.tabControl1.TabIndex = 0;
-            // 
-            // tbsignup
-            // 
-            this.tbsignup.Controls.Add(this.groupBox3);
-            this.tbsignup.Location = new System.Drawing.Point(4, 25);
-            this.tbsignup.Name = "tbsignup";
-            this.tbsignup.Padding = new System.Windows.Forms.Padding(3);
-            this.tbsignup.Size = new System.Drawing.Size(341, 221);
-            this.tbsignup.TabIndex = 1;
-            this.tbsignup.Text = "Sign up";
-            this.tbsignup.UseVisualStyleBackColor = true;
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.btnCreateAcctb2);
-            this.groupBox3.Controls.Add(this.btnBrowsetb2);
-            this.groupBox3.Controls.Add(this.txtfoldertb2);
-            this.groupBox3.Controls.Add(this.label3);
-            this.groupBox3.Controls.Add(this.txtpasstb2);
-            this.groupBox3.Controls.Add(this.txtusernametb2);
-            this.groupBox3.Controls.Add(this.label2);
-            this.groupBox3.Controls.Add(this.label1);
-            this.groupBox3.Location = new System.Drawing.Point(6, 7);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(329, 192);
-            this.groupBox3.TabIndex = 8;
-            this.groupBox3.TabStop = false;
-            // 
-            // btnCreateAcctb2
-            // 
-            this.btnCreateAcctb2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnCreateAcctb2.Location = new System.Drawing.Point(20, 126);
-            this.btnCreateAcctb2.Name = "btnCreateAcctb2";
-            this.btnCreateAcctb2.Size = new System.Drawing.Size(288, 38);
-            this.btnCreateAcctb2.TabIndex = 15;
-            this.btnCreateAcctb2.Text = "Create Account";
-            this.btnCreateAcctb2.UseVisualStyleBackColor = true;
-            this.btnCreateAcctb2.Click += new System.EventHandler(this.btnCreateAcctb2_Click);
-            // 
-            // btnBrowsetb2
-            // 
-            this.btnBrowsetb2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnBrowsetb2.Location = new System.Drawing.Point(233, 84);
-            this.btnBrowsetb2.Name = "btnBrowsetb2";
-            this.btnBrowsetb2.Size = new System.Drawing.Size(75, 23);
-            this.btnBrowsetb2.TabIndex = 14;
-            this.btnBrowsetb2.Text = "Browse";
-            this.btnBrowsetb2.UseVisualStyleBackColor = true;
-            this.btnBrowsetb2.Click += new System.EventHandler(this.btnBrowsetb2_Click_1);
-            // 
-            // txtfoldertb2
-            // 
-            this.txtfoldertb2.Location = new System.Drawing.Point(117, 86);
-            this.txtfoldertb2.Name = "txtfoldertb2";
-            this.txtfoldertb2.Size = new System.Drawing.Size(100, 20);
-            this.txtfoldertb2.TabIndex = 13;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 89);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 13);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "Choose Folder";
-            // 
-            // txtpasstb2
-            // 
-            this.txtpasstb2.Location = new System.Drawing.Point(117, 58);
-            this.txtpasstb2.Name = "txtpasstb2";
-            this.txtpasstb2.PasswordChar = '*';
-            this.txtpasstb2.Size = new System.Drawing.Size(100, 20);
-            this.txtpasstb2.TabIndex = 11;
-            // 
-            // txtusernametb2
-            // 
-            this.txtusernametb2.Location = new System.Drawing.Point(117, 32);
-            this.txtusernametb2.Name = "txtusernametb2";
-            this.txtusernametb2.Size = new System.Drawing.Size(100, 20);
-            this.txtusernametb2.TabIndex = 10;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 61);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Choose Pass";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "User Name";
             // 
             // tbsignin
             // 
@@ -178,7 +81,7 @@
             this.tbsignin.Location = new System.Drawing.Point(4, 25);
             this.tbsignin.Name = "tbsignin";
             this.tbsignin.Padding = new System.Windows.Forms.Padding(3);
-            this.tbsignin.Size = new System.Drawing.Size(341, 221);
+            this.tbsignin.Size = new System.Drawing.Size(352, 254);
             this.tbsignin.TabIndex = 0;
             this.tbsignin.Text = "Sign in";
             this.tbsignin.UseVisualStyleBackColor = true;
@@ -272,6 +175,105 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // tbsignup
+            // 
+            this.tbsignup.Controls.Add(this.groupBox3);
+            this.tbsignup.Location = new System.Drawing.Point(4, 25);
+            this.tbsignup.Name = "tbsignup";
+            this.tbsignup.Padding = new System.Windows.Forms.Padding(3);
+            this.tbsignup.Size = new System.Drawing.Size(352, 254);
+            this.tbsignup.TabIndex = 1;
+            this.tbsignup.Text = "Sign up";
+            this.tbsignup.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.btnCreateAcctb2);
+            this.groupBox3.Controls.Add(this.btnBrowsetb2);
+            this.groupBox3.Controls.Add(this.txtfoldertb2);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.txtpasstb2);
+            this.groupBox3.Controls.Add(this.txtusernametb2);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(6, 7);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(338, 231);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            // 
+            // btnCreateAcctb2
+            // 
+            this.btnCreateAcctb2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCreateAcctb2.Location = new System.Drawing.Point(20, 126);
+            this.btnCreateAcctb2.Name = "btnCreateAcctb2";
+            this.btnCreateAcctb2.Size = new System.Drawing.Size(288, 38);
+            this.btnCreateAcctb2.TabIndex = 15;
+            this.btnCreateAcctb2.Text = "Create Account";
+            this.btnCreateAcctb2.UseVisualStyleBackColor = true;
+            this.btnCreateAcctb2.Click += new System.EventHandler(this.btnCreateAcctb2_Click);
+            // 
+            // btnBrowsetb2
+            // 
+            this.btnBrowsetb2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBrowsetb2.Location = new System.Drawing.Point(233, 84);
+            this.btnBrowsetb2.Name = "btnBrowsetb2";
+            this.btnBrowsetb2.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowsetb2.TabIndex = 14;
+            this.btnBrowsetb2.Text = "Browse";
+            this.btnBrowsetb2.UseVisualStyleBackColor = true;
+            this.btnBrowsetb2.Click += new System.EventHandler(this.btnBrowsetb2_Click_1);
+            // 
+            // txtfoldertb2
+            // 
+            this.txtfoldertb2.Location = new System.Drawing.Point(117, 86);
+            this.txtfoldertb2.Name = "txtfoldertb2";
+            this.txtfoldertb2.Size = new System.Drawing.Size(100, 20);
+            this.txtfoldertb2.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Choose Folder";
+            // 
+            // txtpasstb2
+            // 
+            this.txtpasstb2.Location = new System.Drawing.Point(117, 58);
+            this.txtpasstb2.Name = "txtpasstb2";
+            this.txtpasstb2.PasswordChar = '*';
+            this.txtpasstb2.Size = new System.Drawing.Size(100, 20);
+            this.txtpasstb2.TabIndex = 11;
+            // 
+            // txtusernametb2
+            // 
+            this.txtusernametb2.Location = new System.Drawing.Point(117, 32);
+            this.txtusernametb2.Name = "txtusernametb2";
+            this.txtusernametb2.Size = new System.Drawing.Size(100, 20);
+            this.txtusernametb2.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Choose Pass";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "User Name";
+            // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
@@ -281,17 +283,28 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(11, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(343, 258);
+            this.pictureBox1.Size = new System.Drawing.Size(351, 273);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Enabled = false;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(42, 192);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "label7";
             // 
             // Form1
             // 
             this.AcceptButton = this.btnSignintb1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(361, 541);
+            this.ClientSize = new System.Drawing.Size(378, 576);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -299,13 +312,13 @@
             this.Name = "Form1";
             this.Text = "DBLike Client";
             this.tabControl1.ResumeLayout(false);
-            this.tbsignup.ResumeLayout(false);
-            this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
             this.tbsignin.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
+            this.tbsignup.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -336,6 +349,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
     }
 }
 
