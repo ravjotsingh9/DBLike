24ecda170c70dfb200b93807f04aac8cb0c9a438
hahnmiao <hahnmiao@gmail.com>
2014-08-02 13:06:39 -0700

    add file list

---
+++ DBLike/Client/LocalFileSysAccess/FileList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.LocalFileSysAccess
+{
+
+    // for every file
+    public class FileInfo
+    {
+        public DateTime time;
+        public string md5r;
+    }
+
+
+    // local file list to store hashvalue and time stamp
+    public static class FileList
+    {
+        public static string fullpathOfChnagedFile;
+
+        // dic to store file timestamp and hashvalue
+        public static Dictionary<string, FileInfo> fileInfoDic = new Dictionary<string, FileInfo>();
+
+
+    }
+}
---
+++ DBLike/ClientUnitTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Server.BlobAccess;
+using Client.MessageClasses;
+using Server.MessageClasses;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Server.ConnectionManager;
+using Client;
+
+namespace ClientUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void dicTest()
+        {
+            Client.LocalFileSysAccess.FileInfo tmp = new Client.LocalFileSysAccess.FileInfo();
+            tmp.time = DateTime.UtcNow;
+            tmp.md5r = "hashvalue";
+
+
+            Client.LocalFileSysAccess.FileInfo tmp2 = new Client.LocalFileSysAccess.FileInfo();
+            tmp2.time = DateTime.UtcNow;
+            tmp2.md5r = "hashvalue";
+
+            Client.LocalFileSysAccess.FileList.fileInfoDic.Add("c:\\", tmp);
+            Client.LocalFileSysAccess.FileList.fileInfoDic.Add("c:\\test", tmp2);
+
+            string str = "";
+        }
+    }
+}
