2b042adcafc6cb910234aaefb4d76e1b3d1e950f
elmali <leevalve@gmail.com>
2014-08-03 23:54:33 -0700

    blob sync modified 2nd

--- DBLike/Server/BlobAccess/blobCopy.cs
+++ DBLike/Server/BlobAccess/blobCopy.cs
@@ -59,36 +59,57 @@ namespace Server.BlobAccess
             {
                 return;
             }
-
-            foreach (var srcBlob in srcCloudBlobContainer.ListBlobs())
+            
+            foreach (var srcCloudBlobContainer in srcBlobClient.ListContainers())
             {
-                if (srcBlob.GetType() == typeof(CloudBlockBlob))
+                if (srcCloudBlobContainer.Name != "vhds")
                 {
-                    var srcBlockBlock = (CloudBlockBlob)srcBlob;
-                    var dstBlockBlock = dstCloudBlobContainer
-                        .GetBlockBlobReference(srcBlockBlock.Name);
-                    srcBlockBlock.FetchAttributes();
+                    var dstCloudBlobContainer = dstBlobClient.GetContainerReference(srcCloudBlobContainer.Name);
 
-                    if (!dstBlockBlock.Exists())
+                    dstCloudBlobContainer.CreateIfNotExists();
+
+                    //Assuming the source blob container ACL is "Private", let's create a Shared Access Signature with
+                    //Start Time = Current Time (UTC) - 15 minutes to account for Clock Skew
+                    //Expiry Time = Current Time (UTC) + 7 Days - 7 days is the maximum time allowed for copy operation to finish.
+                    //Permission = Read so that copy service can read the blob from source
+                    var sas = srcCloudBlobContainer.GetSharedAccessSignature(new SharedAccessBlobPolicy()
                     {
-                        //Create a SAS URI for the blob
-                        var srcBlockBlobSasUri = string.Format("{0}{1}", srcBlockBlock.Uri, sas);
-                        // throws exception StorageException:
-                        // The remote server returned an error: (404) Not Found.
-                        dstBlockBlock.StartCopyFromBlob(new Uri(srcBlockBlobSasUri));
-                        dstBlockBlock.CreateSnapshot();
-                    }
-                    else
+                        SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-15),
+                        SharedAccessExpiryTime = DateTime.UtcNow.AddDays(7),
+                        Permissions = SharedAccessBlobPermissions.Read,
+                    });
+                    foreach (var srcBlob in srcCloudBlobContainer.ListBlobs())
                     {
-                        dstBlockBlock.FetchAttributes();
-                        if ((dstBlockBlock.Metadata["hashValue"] != srcBlockBlock.Metadata["hashValue"]) || ((dstBlockBlock.Metadata["timestamp"] != srcBlockBlock.Metadata["timestamp"])))
+                        if (srcBlob.GetType() == typeof(CloudBlockBlob))
                         {
-                            //Create a SAS URI for the blob
-                            var srcBlockBlobSasUri = string.Format("{0}{1}", srcBlockBlock.Uri, sas);
-                            // throws exception StorageException:
-                            // The remote server returned an error: (404) Not Found.
-                            dstBlockBlock.StartCopyFromBlob(new Uri(srcBlockBlobSasUri));
-                            dstBlockBlock.CreateSnapshot();
+                            var srcBlockBlock = (CloudBlockBlob)srcBlob;
+                            var dstBlockBlock = dstCloudBlobContainer
+                                .GetBlockBlobReference(srcBlockBlock.Name);
+                            srcBlockBlock.FetchAttributes();
+
+                            if (!dstBlockBlock.Exists())
+                            {
+                                //Create a SAS URI for the blob
+                                var srcBlockBlobSasUri = string.Format("{0}{1}", srcBlockBlock.Uri, sas);
+                                // throws exception StorageException:
+                                // The remote server returned an error: (404) Not Found.
+                                dstBlockBlock.StartCopyFromBlob(new Uri(srcBlockBlobSasUri));
+                                dstBlockBlock.CreateSnapshot();
+                            }
+                            else
+                            {
+                                dstBlockBlock.FetchAttributes();
+                                if ((dstBlockBlock.Metadata["hashValue"] != srcBlockBlock.Metadata["hashValue"]) || ((dstBlockBlock.Metadata["timestamp"] != srcBlockBlock.Metadata["timestamp"])))
+                                {
+                                    //Create a SAS URI for the blob
+                                    var srcBlockBlobSasUri = string.Format("{0}{1}", srcBlockBlock.Uri, sas);
+                                    // throws exception StorageException:
+                                    // The remote server returned an error: (404) Not Found.
+                                    dstBlockBlock.StartCopyFromBlob(new Uri(srcBlockBlobSasUri));
+                                    dstBlockBlock.CreateSnapshot();
+                                }
+                            }
+
                         }
                     }
 
