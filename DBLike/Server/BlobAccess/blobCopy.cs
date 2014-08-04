51ab6aea85fa1a23e2044d97bfdcc6ec172af896
elmali <leevalve@gmail.com>
2014-08-03 23:51:10 -0700

    blob sync modified

--- DBLike/Server/BlobAccess/blobCopy.cs
+++ DBLike/Server/BlobAccess/blobCopy.cs
@@ -59,37 +59,36 @@ namespace Server.BlobAccess
             {
                 return;
             }
-            
-            foreach (var srcCloudBlobContainer in srcBlobClient.ListContainers())
+
+            foreach (var srcBlob in srcCloudBlobContainer.ListBlobs())
             {
-                if (srcCloudBlobContainer.Name != "vhds")
+                if (srcBlob.GetType() == typeof(CloudBlockBlob))
                 {
-                    var dstCloudBlobContainer = dstBlobClient.GetContainerReference(srcCloudBlobContainer.Name);
+                    var srcBlockBlock = (CloudBlockBlob)srcBlob;
+                    var dstBlockBlock = dstCloudBlobContainer
+                        .GetBlockBlobReference(srcBlockBlock.Name);
+                    srcBlockBlock.FetchAttributes();
 
-                    dstCloudBlobContainer.CreateIfNotExists();
-
-                    //Assuming the source blob container ACL is "Private", let's create a Shared Access Signature with
-                    //Start Time = Current Time (UTC) - 15 minutes to account for Clock Skew
-                    //Expiry Time = Current Time (UTC) + 7 Days - 7 days is the maximum time allowed for copy operation to finish.
-                    //Permission = Read so that copy service can read the blob from source
-                    var sas = srcCloudBlobContainer.GetSharedAccessSignature(new SharedAccessBlobPolicy()
+                    if (!dstBlockBlock.Exists())
                     {
-                        SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-15),
-                        SharedAccessExpiryTime = DateTime.UtcNow.AddDays(7),
-                        Permissions = SharedAccessBlobPermissions.Read,
-                    });
-                    foreach (var srcBlob in srcCloudBlobContainer.ListBlobs())
+                        //Create a SAS URI for the blob
+                        var srcBlockBlobSasUri = string.Format("{0}{1}", srcBlockBlock.Uri, sas);
+                        // throws exception StorageException:
+                        // The remote server returned an error: (404) Not Found.
+                        dstBlockBlock.StartCopyFromBlob(new Uri(srcBlockBlobSasUri));
+                        dstBlockBlock.CreateSnapshot();
+                    }
+                    else
                     {
-                        if (srcBlob.GetType() == typeof(CloudBlockBlob))
+                        dstBlockBlock.FetchAttributes();
+                        if ((dstBlockBlock.Metadata["hashValue"] != srcBlockBlock.Metadata["hashValue"]) || ((dstBlockBlock.Metadata["timestamp"] != srcBlockBlock.Metadata["timestamp"])))
                         {
-                            var srcBlockBlock = (CloudBlockBlob)srcBlob;
-                            var dstBlockBlock = dstCloudBlobContainer
-                                .GetBlockBlobReference(srcBlockBlock.Name);
                             //Create a SAS URI for the blob
                             var srcBlockBlobSasUri = string.Format("{0}{1}", srcBlockBlock.Uri, sas);
                             // throws exception StorageException:
                             // The remote server returned an error: (404) Not Found.
                             dstBlockBlock.StartCopyFromBlob(new Uri(srcBlockBlobSasUri));
+                            dstBlockBlock.CreateSnapshot();
                         }
                     }
 
