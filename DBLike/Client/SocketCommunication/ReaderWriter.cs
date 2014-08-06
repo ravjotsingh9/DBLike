2c2133edfa2179f30632fcd916443a6df04b3d1f
hahnmiao <hahnmiao@gmail.com>
2014-08-06 00:50:04 -0700

    change back to ASCII

--- DBLike/Client/SocketCommunication/ReaderWriter.cs
+++ DBLike/Client/SocketCommunication/ReaderWriter.cs
@@ -20,44 +20,11 @@ namespace Client.SocketCommunication
             try
             {
                 soc.ReceiveTimeout = 10000;
-                //byte[] tmp = new byte[1024];
-                //soc.Receive(tmp);
+                byte[] tmp = new byte[1024];
+                soc.Receive(tmp);
 
-
-                // Read the  message sent by the server.
-                // The end of the message is signaled using the
-                // "<EOF>" marker.
-                byte[] buffer = new byte[2048];
-                StringBuilder messageData = new StringBuilder();
-                int bytes = -1;
-
-                do
-                {
-                    bytes = soc.Receive(buffer);
-
-                    //bytes = sslStream.Read(buffer, 0, buffer.Length);
-
-                    // Use Decoder class to convert from bytes to UTF8
-                    // in case a character spans two buffers.
-                    Decoder decoder = Encoding.UTF8.GetDecoder();
-                    char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
-                    decoder.GetChars(buffer, 0, bytes, chars, 0);
-
-
-                    messageData.Append(chars);
-                    // Check for EOF.
-                    if (messageData.ToString().IndexOf("<EOF>") != -1)
-                    {
-                        break;
-                    }
-                } while (bytes != 0);
-
-
-                // get the msg
-                return messageData.ToString();
-
-                //string str = System.Text.Encoding.ASCII.GetString(tmp);
-                //return str;
+                string str = System.Text.Encoding.ASCII.GetString(tmp);
+                return str;
             }
             catch (SocketException e)
             {
@@ -72,15 +39,13 @@ namespace Client.SocketCommunication
                 return null;
             }
         }
-
-
         //TBD socket writer function
         [MethodImpl(MethodImplOptions.Synchronized)]
         public bool writetoSocket(Socket soc, String message)
         {
             try
             {
-                byte[] msg = Encoding.UTF8.GetBytes(message);
+                byte[] msg = Encoding.ASCII.GetBytes(message);
                 soc.SendTimeout = 10000;
                 int bytesSent = soc.Send(msg);
                 return true;
